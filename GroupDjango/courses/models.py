d3192bbaef7cc237671aea40e389f90e1b70f2a4
ludo <ludobourneville@gmail.com>
2022-09-01 10:26:19 +1200

    __str__ models
    added methods to represent models instead of default id

--- GroupDjango/courses/models.py
+++ GroupDjango/courses/models.py
@@ -11,11 +11,17 @@ class Course(models.model):
     howDescription = models.modelsTextField(max_length=500)
     price= models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
 
 class Topic(models.model):
     course = models.ForeignKey(Course, on_delete= models.CASCADE)
     name = models.CharField(max_length=50)
     intro = models.modelsTextField(max_length=500)
+
+    def __str__(self):
+        str = self.course + self.name
+        return str
 
 class Content(models.model):
     topic = models.ForeignKey(Topic, on_delete= models.CASCADE)
@@ -23,7 +29,14 @@ class Content(models.model):
     explanation = models.modelsTextField(max_length=500)
     example = models.modelsTextField(max_length=500)
 
+    def __str__(self):
+        return self.name
+
 class Questions(models.model):
     content = models.ForeignKey(Content, on_delete= models.CASCADE)
     question = models.CharField(max_length=50)
     answer = models.BooleanField()
+
+    def __str__(self):
+        str = self.id + self.name
+        return str
