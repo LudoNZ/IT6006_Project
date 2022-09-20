c045fefba38f37f62c7028373e7cc44936a0a1ab
masamist <msm-k0304@hotmail.com>
2022-09-21 11:20:39 +1200

    Styling Question card and course forms

--- GroupDjango/courses/views.py
+++ GroupDjango/courses/views.py
@@ -13,114 +13,129 @@ from enrolment.models import Result
 
 # Create your views here.
 
+
 class DeveloperPageView(TemplateView):
     template_name = "courses/developer.html"
+
 
 class CourseListView(ListView):
     model = Course
     template_name = "courses/course_list.html"
 
+
 class CourseDetailView(DetailView):
     model = Course
     template_name = "courses/course_detail.html"
 
+
 class CourseNewDetailView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     permission_required = ("courses.add_courses")
     model = Course
-    template_name = "courses/course_add.html"
-    success_url="../../../courses/list/"
-    fields =["name", "whatHeading", "whatDescription", "howHeading", "howDescription", "price"]
+    #template_name = "courses/course_add.html"
+    success_url = "../../../course/courses/list/"
+    fields = ["name", "whatHeading", "whatDescription",
+              "howHeading", "howDescription", "price"]
+
 
 class CourseEditDetailView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     permission_required = ("courses.change_courses")
     model = Course
-    template_name = "courses/course_edit.html"
-    success_url="../../../courses/list/"
-    fields =["name", "whatHeading", "whatDescription", "howHeading", "howDescription", "price"]
+    #template_name = "courses/course_edit.html"
+    success_url = "../../../course/courses/list/"
+    fields = ["name", "whatHeading", "whatDescription",
+              "howHeading", "howDescription", "price"]
+
 
 class CourseDeleteDetailView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     permission_required = ("courses.delete_courses")
     model = Course
     template_name = "courses/course_delete.html"
-    success_url=reverse_lazy("courses")
+    success_url = reverse_lazy("courses")
+
 
 class TopicListView(DetailView):
     model = Course
     template_name = "courses/topic_list.html"
 
+
 class TopicNewDetailView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     permission_required = ("courses.add_topics")
     model = Topic
     template_name = "courses/topic_add.html"
-    success_url="../../../courses/list/"
-    fields =["course", "name", "intro"]
+    success_url = "../../../courses/list/"
+    fields = ["course", "name", "intro"]
+
 
 class TopicDetailView(DetailView):
     model = Topic
     template_name = "courses/topic_detail.html"
 
+
 class TopicEditDetailView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     permission_required = ("courses.change_courses")
     model = Topic
     template_name = "courses/topic_edit.html"
-    success_url="../../../courses/list/"
-    fields =["course", "name", "intro"]
+    success_url = "../../../courses/list/"
+    fields = ["course", "name", "intro"]
+
 
 class TopicDeleteDetailView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView, DetailView):
     permission_required = ("courses.delete_courses")
     model = Topic
     template_name = "courses/topic_delete.html"
-    success_url=reverse_lazy("courses")
+    success_url = reverse_lazy("courses")
+
 
 class ContentDetailView(DetailView):
     model = Content
     template_name = "courses/content_detail.html"
-    
+
     def get(self, request, pk):
         content = Content.objects.get(pk=pk)
         user_results = Result.objects.filter(user=request.user).all()
-        return render(request, 'courses/content_detail.html', {'user_results': user_results, 
-                                                                'content': content})
+        return render(request, 'courses/content_detail.html', {'user_results': user_results,
+                                                               'content': content})
 
 
 class ContentNewDetailView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     permission_required = ("courses.add_topics")
     model = Content
     template_name = "courses/content_add.html"
-    success_url="../../../courses/list/"
-    fields =["topic", "name", "explanation", "example", "minutesToComplete"]
+    success_url = "../../../courses/list/"
+    fields = ["topic", "name", "explanation", "example", "minutesToComplete"]
+
 
 def answer_true(request, id, pk):
     question = get_object_or_404(Question, id=id)
-        
+
     if question.answer:
         result = Result.objects.update_or_create(
-                        user=request.user, 
-                        question=question,
-                        defaults={'result': True})
+            user=request.user,
+            question=question,
+            defaults={'result': True})
     else:
         result = Result.objects.update_or_create(
-                        user=request.user, 
-                        question=question,
-                        defaults={'result': False})
+            user=request.user,
+            question=question,
+            defaults={'result': False})
     result.save()
 
     return JsonResponse({'is_correct': result.result})
 
+
 def answer_false(request, id, pk):
     question = get_object_or_404(Question, id=id)
-    
 
     if not question.answer:
         result = Result.objects.update_or_create(
-                        user=request.user, 
-                        question=question,
-                        defaults={'result': True})
+            user=request.user,
+            question=question,
+            defaults={'result': True})
     else:
         result = Result.objects.update_or_create(
-                        user=request.user, 
-                        question=question,
-                        defaults={'result': False})
+            user=request.user,
+            question=question,
+            defaults={'result': False})
 
     result.save()
 
